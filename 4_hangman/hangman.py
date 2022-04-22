7f5c2be034b4cb93a66dd426b370ba79574eb35b
MaxKoch1996 <MaxKochGolden1996@gmail.com>
2022-04-22 15:01:31 +0300

    update

--- 4_hangman/hangman.py
+++ 4_hangman/hangman.py
@@ -29,6 +29,10 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            # else:
+            #     lives = lives - 1 # takes away a life if wrong
+            #     print('Letter is not in word.')
+        
         
         elif user_letter in used_letters:
             print('You have already used that character. Please try again')
@@ -37,8 +41,12 @@ def hangman():
             print('Invalid character. Please try again.')
     
     # gets here when len(word_letters) == 0
+    # if lives == 0:
+    #     print('You died. The word was', word)
+    # else:
+    #     print('You guesssed the word', word, '!!')
 
-print(hangman())
+hangman()
 
 
 
